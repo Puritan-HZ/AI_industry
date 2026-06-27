2d3afa4002e4b0843499be821789d03dbc0d9db6
Puritan <2049682157@qq.com>
2026-06-27 16:28:28 +0800

    Delete NWEW directory

--- NWEW/硬件驱动源码/all.h
+++
@@ -1,3 +0,0 @@
-extern char *sensor(int *temp, int *humi, float *illum);
-extern int LED_Open(int on_off);
-extern int SEmotor(int angle);
--- NWEW/硬件驱动源码/functions.h
+++
@@ -1,8 +0,0 @@
-// functions.h
-
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-void getMultipleValues(int *a, float *b, char *c);
-
-#endif /* FUNCTIONS_H */
